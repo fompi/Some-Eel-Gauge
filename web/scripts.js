fabc25e46fefe12455803d76696872085ab35991
impshum <impshum@sausage.local>
2019-02-26 10:55:29 +0000

    Update scripts.js

--- web/scripts.js
+++ web/scripts.js
@@ -1,11 +1,11 @@
 eel.expose(say_hello_js); // Expose this function to Python
+
 function say_hello_js(x) {
   console.log("Hello from " + x);
 }
 
 say_hello_js("Javascript World!");
 eel.say_hello_py("Javascript World!"); // Call a Python function
-
 
 var opts = {
   lines: 300,
